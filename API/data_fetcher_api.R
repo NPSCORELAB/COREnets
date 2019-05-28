c57e70c83a702e07d04d9177a00ab5ac6bda9711
cjcallag <cjcallag@nps.edu>
2019-05-28 15:56:50 -0700

    add new functions

--- API/data_fetcher_api.R
+++ API/data_fetcher_api.R
@@ -1,6 +1,7 @@
 library(plumber)
 library(COREnets)
 library(igraph)
+library(DT)
 
 #* Echo back the input for testing
 #* @param msg The message to echo
@@ -38,6 +39,33 @@ function(id){
     g <- graph_from_data_frame(listed_dat$edges,
                                vertices=listed_dat$nodes)
     visNetwork::visIgraph(g)
+  }
+  else{
+    paste0("The dataset name provided was invalid, use the `list_data` parameter to list the data names.")
+  }
+}
+
+
+#* @param id
+#* @get /get_network_stats
+#* @serializer htmlwidget
+function(id){
+  id <- as.character(id)
+  valid <- data(package="COREnets")$results[,3]
+  if(id %in% valid){
+    listed_dat <- get(data(list=id))
+    g <- graph_from_data_frame(listed_dat$edges,
+                               vertices=listed_dat$nodes) %>%
+      igraph::set.graph.attribute("density", edge_density(.)) %>%
+      igraph::set.graph.attribute("avg_degree", mean(degree(.))) %>%
+      igraph::set.graph.attribute("avg_clu_coef", transitivity(., "average")) 
+    
+    data.frame(
+      "Density" = graph_attr(g, "density"),
+      "Avg. Degree" = graph_attr(g, "avg_degree"),
+      "Avg. Clustering Coefficient" = graph_attr(g, "avg_degree")
+    ) %>%
+      DT::datatable()
   }
   else{
     paste0("The dataset name provided was invalid, use the `list_data` parameter to list the data names.")
--- dockerfile
+++ dockerfile
@@ -1,5 +1,6 @@
 FROM rocker/r-ver:3.5.0
 
+## Install Secure Sockets Layer Toolkit and Development Files and Documentation for libcurl
 RUN apt-get update -qq && apt-get install -y \
       libssl-dev \
       libcurl4-gnutls-dev
@@ -7,13 +8,15 @@ RUN apt-get update -qq && apt-get install -y \
 ## Install Padoc
 Run apt-get install pandoc -y
 
-## Install R packages
+## Install R packages from CRAN
 RUN R -e "install.packages('plumber')"
 RUN R -e "install.packages('dplyr')"
 RUN R -e "install.packages('remotes')"
 RUN R -e "install.packages('igraph')"
 RUN R -e "install.packages('visNetwork')"
 RUN R -e "install.packages('DT')"
+
+## Install R packages from Github
 RUN installGithub.r NPSCORELAB/COREnets
 
 COPY / /
