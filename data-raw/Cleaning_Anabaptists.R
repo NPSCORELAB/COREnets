01c97203b899dac092dc72e4fe131a47f34632a7
cjcallag <cjcallag@nps.edu>
2019-06-11 09:51:51 -0700

    Add BAAD

--- data-raw/Cleaning_Anabaptists.R
+++ data-raw/Cleaning_Anabaptists.R
@@ -1,77 +1,159 @@
-###################################
-# Transforming the Anabaptist Data
-# Date: 12/28/2018
-###################################
-library(igraph)
-library(dplyr)
 
-# Get the data from the Raw Data > Anabaptists folder:
-anabaptists <- read.graph(file="datasets/Anabaptist Leaders.net", format = "pajek")
-# Now get the attributes from the same location:
-anabaptists_attr <- read.csv(file="datasets/Anabaptist Attributes.csv", header = T, as.is = T)
-# Clean up the first column's title:
-anabaptists_attr <- anabaptists_attr %>%
-  rename(id=X)
-# Add attributes to igraph:
-# First test that all vertex names are identical
-test <- get.data.frame(anabaptists, what="vertices")
-anabaptists_attr$id==test$id
-# As it turns out, observation 61 (test$id[61] | anabaptists_attr$id[61]) are not identical. So recoding is needed:
-V(anabaptists)$id <- ifelse(V(anabaptists)$id=="Boekbinder","Gerrit Boekbinder" ,V(anabaptists)$id)
-V(anabaptists)$name <- ifelse(V(anabaptists)$name=="Boekbinder","Gerrit Boekbinder" ,V(anabaptists)$name)
-# Now we can add attributes:
-anabaptists_full <- graph_from_data_frame(get.data.frame(anabaptists, what="edges"),
-                              vertices = anabaptists_attr)
+library(tidyverse)
 
-# Node list: 
-nodes <- as_tibble(get.data.frame(anabaptists_full, what="vertices"))
+# paper: https://apps.dtic.mil/dtic/tr/fulltext/u2/a632471.pdf
 
-# Edge list:
-edges <- as_tibble(get.data.frame(anabaptists_full, what="edges"))
+# helper vars ==========================================================================
+# key-value maps to recode `Origin #` and `Operate #`
+kv_origins <- c(
+  "1" = "Austria",
+  "2" = "France",
+  "3" = "Germany",
+  "4" = "Italy",
+  "5" = "Moravia",
+  "6" = "Netherlands",
+  "7" = "Poland",
+  "8" = "Switzerland"
+)
 
-# Dataset metadata:
+kv_bases <- c(
+  "1" = "Austria",
+  "2" = "Germany",
+  "3" = "Moravia",
+  "4" = "Netherlands",
+  "5" = "Switzerland"
+)
+
+# read "raw" data ======================================================================
+init_attrs <- read_csv("datasets/Anabaptist Attributes.csv")
+
+# clean node attributes ================================================================
+groups_attr <- init_attrs %>%
+  rename(name = X1) %>% 
+  mutate(is_anabaptist = Anabaptist == 1) %>% 
+  # keep name and group cols
+  select(name, is_anabaptist, Melchiorite, `Swiss Brethren`, `Other Anabaptist`,
+         Lutheran, Reformed, `Other Protestant`, Denck, Hut, Hutterite) %>% 
+  # melt data frame
+  gather(group, val, -name, -is_anabaptist) %>% 
+  # keep "TRUE" rows
+  filter(val == 1) %>% 
+  # drop "TRUE"/"FALSE" column
+  select(-val)
+
+nodes_df <- init_attrs %>% 
+  # use "better" names
+  rename(
+    name = X1,
+    is_violence_sanctioning = Violence,            # paper: p. 47
+    is_apocalyptic          = Apocalyptic,         # paper: p. 47
+    is_believer_baptist     = `Believers Baptism`, # paper: p. 2
+    origin                  = `Origin #`,
+    based_in                = `Operate #`,
+    is_munster_rebel        = `Münster Rebellion`
+  ) %>% 
+  select(
+    -c(Anabaptist, Melchiorite, `Swiss Brethren`, `Other Anabaptist`,
+       Lutheran, Reformed, `Other Protestant`, Denck, Hut, Hutterite,
+       Tradition) # `Tradition` is the same as `group`
+  ) %>% 
+  # coerce binary vars to correct type
+  mutate_at(vars(starts_with("is_")), as.logical) %>% 
+  # recode categorical variables
+  mutate(origin = recode(origin, !!!kv_origins),
+         based_in = recode(based_in, !!!kv_bases)) %>%
+  # attach attribute containing all groups
+  left_join(groups_attr, by = "name") %>%
+  # reorder columns
+  select(name, group, origin, based_in, is_anabaptist, everything())
+
+# pull edges from pajek file
+edges_df <- igraph::read_graph("datasets/Anabaptist Leaders.net", format = "paj") %>%
+  igraph::as_data_frame() %>% 
+  select(-weight) %>% 
+  # fix name that doesn't match
+  mutate_all(~ if_else(. == "Boekbinder", "Gerrit Boekbinder", .)) %>% 
+  # pajek file is tracking edges in both directions, but it's supposed to be undirected
+  distinct() %>% 
+  rename(source = from, target = to)
+
+# build igraph object ==================================================================
+g <- igraph::graph_from_data_frame(
+  d = edges_df,
+  directed = FALSE,
+  vertices = nodes_df
+)
+
+# build final dataset ==================================================================
+.network <- list(
+  metadata = list(
+    is_directed  = igraph::is_directed(g),
+    is_weighted  = igraph::is_weighted(g),
+    is_multiplex = igraph::any_multiple(g),
+    node_type    = "people",
+    is_two_mode  = igraph::is_bipartite(g),
+    is_dynamic   = FALSE,
+    are_nodes_spatial = inherits(igraph::as_data_frame(g, what = "vertices"), "sf"),
+    are_edges_spatial =  inherits(igraph::as_data_frame(g, what = "edges"), "sf")
+  ),
+  node_table = as_tibble(igraph::as_data_frame(g, what = "vertices")),
+  edge_table = as_tibble(igraph::as_data_frame(g, what = "edges"))
+)
+
+.introduction <- "This dataset examines social networks between early Anabaptist leaders. 
+Together with the dataset on the beliefs of early Anabaptist leaders and groups, it 
+can be used to examine the diffusion of theology and ideas among early Anabaptist 
+leaders."
+
+.abstract <- "Religiously motivated violence is and always will be a relevant topic. To 
+address and effectively counter contemporary violent groups, it is important to 
+investigate similar historic groups. This thesis attempts to answer the research 
+question: 'During the Radical Reformation, why did some Anabaptist groups accept the 
+use of violence while others did not, and how did the movement evolve to pacifism?' To
+answer this question, this study utilizes a mixed methodology of case study analysis 
+and social network analysis of Anabaptist leaders during the 16th century. This thesis 
+argues that violent ideology is largely a function of three factors: charismatic 
+leadership, isolation, and apocalypticism. The interaction of these factors led to the
+emergence of Anabaptist groups that embraced the use of violence. However, groups' 
+internal characteristics can also lead them away from violence. In the case of the 
+Anabaptists, social proximity assisted leaders with a counter-message to speak 
+effectively to violent ultra-radical factions. The goal of this thesis is to identify 
+characteristics of religious groups that may signal the potential for future violence, 
+while also providing insight into which leaders may be capable of re-directing groups 
+that have become violent."
+
+.bibtex <- c(
+  "@Article{anabaptists_matthews_edmonds_wildman_nunn_2013,
+  title   = {Cultural inheritance or cultural diffusion of religious violence? A quantitative case study of the Radical Reformation}, 
+  volume  = {3}, 
+  DOI     = {10.1080/2153599x.2012.707388}, 
+  number  = {1}, 
+  journal = {Religion, Brain & Behavior}, 
+  author  = {Matthews, Luke J. and Edmonds, Jeffrey and Wildman, Wesley J. and Nunn, Charles L.}, 
+  year    = {2013},
+  pages   = {3-15},
+  }",
+
+  "@MastersThesis{anabaptists_mcLaughlin_2015,
+  author  = {McLaughlin, John M.}, 
+  title   = {Anabaptist Leadership Network},
+  year    = {2015},
+  school  = {Naval Postgraduate School},
+  address = {1 University Circle, Monterey, CA 93943},
+  }"
+)
+
 anabaptists <- list(
   metadata = list(
-    title = "Anabaptist Network",
-    name = "anabaptists",
-    category = "religious",
-    tags = c("religion"),
-    description = NULL
+    title       = "Anabaptist Leadership Network",
+    name        = "anabaptists",
+    category    = "religious",
+    tags        = "religion",
+    description = .introduction,
+    abstract    = .abstract
   ),
-  bibtex_data = list(
-    anabaptists2013= list(preamble = "article",
-         name = "anabaptists2013",
-         title = "Cultural Inheritance or Cultural Diffusion of Religious Violence? A Quantitative Case Study of the Radical Reformation.",
-         author = "Matthews, Luke J., Jeffrey Edmonds, Wesley Wildman, and Charles Nunn.",
-         journal = "Religion, Brain & Behavior.",
-         volume = 3,
-         pages = "3-15",
-         year = 2013),
-    anabaptists2015= list(preamble = "masterthesis",
-         name = "anabaptists2015",
-         title = "Anabaptist Leadership Network.",
-         author = "McLaughlin, John M.",
-         school = "Naval Postgraduate School",
-         address = "1 University Circle, Monterey, CA 93943",
-         year = 2015,
-         month = NULL,
-         note = NULL)
-  ),
-  network = list(
-    net_metadata = list(
-      node_type = "people",
-      edge_type = NULL,
-      modes = 1,
-      directed = FALSE,
-      weighted = FALSE,
-      multiplex = FALSE,
-      dynamic = FALSE,
-      spatial = FALSE
-    ),
-    node_table = nodes,
-    edge_table = edges
-  )
+  bibtex = .bibtex,
+  network = .network
 )
 
 usethis::use_data(anabaptists, overwrite = TRUE)
-
--- data-raw/Cleaning_BAAD.R
+++ data-raw/Cleaning_BAAD.R
@@ -1,65 +1,205 @@
-####################################
-# Cleaning and Structuring BAAD Data
-# Date: 05/20/2019
-####################################
 
 library(igraph)
 library(tidyverse)
 library(COREnets)
+library(sf)
 
+# paper: https://www.jstor.org/stable/10.1017/s0022381608080419?seq=1#metadata_info_tab_contents
+# codebook: https://www.albany.edu/pvc/lethality_paper__CodeBook.pdf
 
-files <- list.files(path="datasets/BAAD/",
-                    pattern = "*.csv", 
-                    full.names = TRUE)
-
-baad_1m <- purrr::map_dfr(files[1], read_csv) %>%
-  as_tibble() %>%
-  COREnets::to_matrix() %>%
-  COREnets::to_graph() %>%
-  get.data.frame("edges")
-
-baad_1m_attr <- purrr::map_dfr(files[2], read_csv) %>%
-  as_tibble()
-
-test_g <- graph_from_data_frame(baad_1m,
-                                vertices = baad_1m_attr)
-
-# Dataset metadata:
-baad <- list(
-  page_metadata = list(
-    title = "Big Allied and Dangerous (BAAD) Network",
-    category = "terrorism",
-    tags = c("terrorism"),
-    about = NULL,
-    description = NULL
-  ),
-  bibtex_data = list(
-    preamble = "misc",
-    title = "Big Allied and Dangerous Dataset Version 2.",
-    author = "Asal, Victor H. and R. Karl Rethemeyer.",
-    publisher = NULL,
-    journal = NULL,
-    volume = NULL,
-    pages = NULL,
-    address = NULL,
-    year = 2015,
-    note = NULL,
-    url = "www.start.umd.edu/baad/database"
-  ),
-  network = list(
-    metadata = list(
-      node_type = "people",
-      edge_type = NULL,
-      modes = 1,
-      directed = TRUE,
-      weighted = FALSE,
-      multiplex = FALSE,
-      dynamic = FALSE,
-      spatial = FALSE
-    ),
-    node_table = as_tibble(get.data.frame(test_g, what="vertices")),
-    edge_table = as_tibble(get.data.frame(test_g, what="edges"))
-  )
+kv_homebase <- c(
+"2"   =   "United States",
+"70"  = "Mexico",
+"91"  = "Honduras",
+"92"  = "El Salvador",
+"93"  = "Nicaragua",
+"100" = "Colombia",
+"101" = "Venezuela",
+"130" = "Ecuador",
+"135" = "Peru" ,
+"145" = "Bolivia",
+"155" = "Chile",
+"160" = "Argentina"  ,
+"200" = "United Kingdom",
+"205" = "Ireland",
+"210" = "Netherlands",
+"220" = "France",
+"225" = "Switzerland",
+"230" = "Spain",
+"255" = "Germany",
+"325" = "Italy",
+"343" = "Macedonia",
+"350" = "Greece",
+"352" = "Cyprus",
+"365" = "Russia",
+"372" = "Georgia",
+"380" = "Sweden",
+"390" = "Denmark",
+"451" = "Sierra Leone",
+"475" = "Nigeria",
+"483" = "Chad",
+"490" = "Zaire",
+"500" = "Uganda",
+"530" = "Ethiopia",
+"531" = "Eritrea",
+"540" = "Angola",
+"560" = "South Africa" ,
+"572" = "Swaziland",
+"600" = "Morocco",
+"615" = "Algeria",
+"620" = "Libya",
+"625" = "Sudan",
+"630" = "Iran",
+"640" = "Turkey",
+"645" = "Iraq",
+"651" = "Egypt",
+"660" = "Lebanon",
+"663" = "Jordan",
+"666" = "Israel",
+"670" = "Saudi Arabia",
+"679" = "Yemen",
+"700" = "Afghanistan",
+"702" = "Tajikistan",
+"704" = "Uzbekistan",
+"710" = "China",
+"740" = "Japan",
+"750" = "India",
+"770" = "Pakistan"   ,
+"771" = "Bangladesh",
+"775" = "Myanmar (Burma)",
+"780" = "Sri Lanka",
+"790" = "Nepal",
+"800" = "Thailand",
+"812" = "Laos",
+"840" = "Philippines",
+"850" ="Indonesia"
 )
-usethis::use_data(baad, overwrite = TRUE)
 
+# read "raw" data ======================================================================
+files <- list.files(path="datasets/BAAD/full/",
+  pattern = "\\.csv$", 
+  full.names = TRUE)
+
+edges <- purrr::map(files, read_csv) %>%
+  # extract from multiple files and pull into one data.frame
+  setNames(str_extract(files,
+     pattern = "[\\w]+\\.csv$")) %>%
+  keep(str_detect(names(.),
+pattern = "edges_")) %>%
+  map(., COREnets::to_matrix) %>%
+  map(., COREnets::to_graph) %>%
+  imap_dfr(., ~.x %>%
+ set.edge.attribute(., name="relationship",
+value=str_extract(.y,
+pattern = "\\w+")) %>%
+ get.data.frame("edges")) %>%
+  mutate(relationship = str_replace(relationship, "edges_", ""))
+
+nodes <- purrr::map(files, read_csv) %>%
+  # extract from multiple files and pull into one data.frame
+  setNames(str_extract(files,
+     pattern = "[\\w]+\\.csv")) %>%
+  keep(str_detect(names(.),
+pattern = "attrs_")) %>%
+  bind_rows()
+
+# clean node attributes ================================================================
+
+nodes <- nodes %>%
+  rename(
+    name    = group,
+    based_in= homecountry,
+    is_contain_ethno    = ContainEthno,
+    is_contain_ethno2   = ContainEthno2,
+    is_contain_relig    = ContainRelig,
+    is_contain_relig2   = ContainRelig2,
+    is_islam= Islam,
+    is_leftist_no_relig_ethno = LeftNoReligEthno,
+    is_terrStrong = terrStrong,
+    is_pure_ethno = PureEthno,
+    is_pure_relig = PureRelig,
+    is_relig_ethno= ReligEthno,
+    is_statespond = statespond,
+    fatalities_1998_2005= fatalities19982005,
+    org_age = OrgAge,
+    org_size= ordsize
+  ) %>%
+  select(-degree) %>%
+  mutate_at(vars(starts_with("is_")), as.logical) %>%
+  mutate(based_in = recode(based_in, !!!kv_homebase))
+
+# build igraph object ==================================================================
+g <- igraph::graph_from_data_frame(
+  d = edges,
+  directed = FALSE,
+  vertices = nodes
+)
+
+# build final dataset ==================================================================
+
+data(countryref, package = "CoordinateCleaner")
+countries <- countryref[!duplicated(countryref[c('name')]),]
+
+nodes <- nodes %>%
+  as_tibble(igraph::as_data_frame(g, what = "vertices")) %>%
+  left_join(countries, by=c("based_in"="name")) %>%
+  as_tibble() %>%
+  st_as_sf(coords = c("centroid.lon", "centroid.lat"), crs = 4326) %>%
+  select(name, based_in, based_in, fatalities_1998_2005, org_age, org_size, is_terrStrong,
+         is_contain_ethno, is_contain_relig, is_contain_ethno2, is_contain_relig2, is_leftist_no_relig_ethno,
+         is_pure_ethno, is_pure_relig, is_islam, geometry) 
+
+.network <- list(
+  metadata = list(
+    is_directed  = igraph::is_directed(g),
+    is_weighted  = igraph::is_weighted(g),
+    is_multiplex = igraph::any_multiple(g),
+    node_type    = "organizations",
+    is_two_mode  = igraph::is_bipartite(g),
+    is_dynamic   = FALSE,
+    are_nodes_spatial = inherits(igraph::as_data_frame(g, what = "vertices"), "sf"),
+    are_edges_spatial =  inherits(igraph::as_data_frame(g, what = "edges"), "sf")
+  ),
+  node_table = nodes,
+  edge_table = as_tibble(igraph::as_data_frame(g, what = "edges"))
+)
+
+.introduction <- "This dataset, Big Allied and Dangerous 1.0 (BAAD1), contains information on the size of terrorist organziations, their ideology,
+whether they are supported by state sponsors, the age of organizations, numbrer of fatalities attributed to the organizations, whether the organization 
+controls territory, and counts of alliace connections."
+
+.abstract <- "Why are some terrorist organizations so much more deadly then others? Rearchers Victor Asal and Karl Rethmeyere address this
+question by examining organizational characteristics such as ideology, size, age, state sponsorship, alliance connections, and control of
+territory while controlling for factors that may also influence lethality, including the political system and relative wealth of the country
+in which the organization is based. Using data from the Memorial Institute for the Prevention of Terrorism’s Terrorism 
+Knowledge Base(TKB), we use a negative binomial model of organizational lethality, finding that organizational size, ideology,
+territorial control, and connectedness are important predictors of lethality while state sponsorship, organizational age, and 
+host country characteristics are not."
+
+.bibtex <- c(
+  "@Article{baad_2008,
+  title   = {The Nature of the Beast: Terrorist Organizational Characteristics and Organizational Lethality.}, 
+  volume  = {70}, 
+  DOI     = {10.1017/s0022381608080419}, 
+  number  = {2}, 
+  journal = {Journal of Politics}, 
+  author  = {Asal, Victor H. and Rethemeyer, R. Karl.}, 
+  year    = {2008},
+  pages   = {437-449},
+  }"
+)
+
+baad <- list(
+  metadata = list(
+    title = "BAAD1 Lethality Dataset",
+    name  = "baad",
+    category    = "terrorism",
+    tags  = "terrorism",
+    description = .introduction,
+    abstract    = .abstract
+  ),
+  bibtex = .bibtex,
+  network = .network
+)
+
+usethis::use_data(baad, overwrite = TRUE)
