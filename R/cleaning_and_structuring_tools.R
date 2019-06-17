47a257d504eba9699e8217eb504b760bca61ec56
CORE Lab <corelab@CORE-Lab.local>
2019-06-16 20:17:06 -0700

    Fix the functions to increase TMI.

--- R/cleaning_and_structuring_tools.R
+++ R/cleaning_and_structuring_tools.R
@@ -1,23 +1,28 @@
-#' @title From Tibble to Adjacency or Incidence Matrix
+#' @title From Dataframe to Adjacency or Incidence Matrix
 #'
 #' @description `to_adj_matrix` returns a `matrix` with the values tibble imported from an excel file.
 #'
 #' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
 #'
 #' @param .df, A `df` produced by the `read_xlsx` function.
+#' @param .rownames, A column value for the row that will be reassigned as rownames and then removed from the data frame.
 #'
 #' @export
-to_matrix <- function(.df) {
+to_matrix <- function(.df, .rownames = 1) {
   if (!is.data.frame(.df)) {
-    stop("Not a dataframe.", call. = FALSE)
+    stop("Not a data frame.", call. = FALSE)
+  }
+  if (!is.numeric(.rownames)) {
+    stop("rownames value provided in not numeric", call. = FALSE)
   }
   
   .df <- as.matrix(.df)
-  row.names(.df) <- .df[ ,1]
-  .df <- .df[ ,-1]
+  row.names(.df) <- .df[ ,.rownames]
+  .df <- .df[ ,-.rownames]
   
-  if (nrow(.df) != ncol(.df)) message("An incidence matrix will be returned")
-  if (nrow(.df) == ncol(.df)) message("An adjacency matrix will be returned.")
+  if (NROW(.df) != NCOL(.df)) message("An incidence matrix will be returned")
+  if (NROW(.df) == NCOL(.df)) message("An adjacency matrix will be returned.")
+  
   .df
 }
 
@@ -31,24 +36,22 @@ to_matrix <- function(.df) {
 #' @importFrom igraph graph_from_adjacency_matrix graph_from_incidence_matrix
 #'
 #' @export
-to_graph <- function(.mat, directed=FALSE, ...){
-  if(is.matrix(.mat)) {
-    if(nrow(.mat) == ncol(.mat)) {
-      if(directed == FALSE){
-        g <- graph_from_adjacency_matrix(.mat, mode = "undirected")
-      }
-      if(directed == TRUE) {
-        g <- graph_from_adjacency_matrix(.mat, mode = "directed")
-      }
-    }
-    if(nrow(.mat)!=ncol(.mat)) {
-      g <- graph_from_incidence_matrix(.mat)
-    }
-  }
-  else{
+to_graph <- function(.mat, .directed=FALSE, ...) {
+  if (!is.matrix(.mat)==TRUE) {
     stop("Object provided is not a matrix.", call. = FALSE)
   }
-  return(g)
+  
+  # Two-mode data
+  if (NROW(.mat) != NCOL(.mat)) {
+    g <- graph_from_incidence_matrix(.mat)
+  }
+  
+  # One-mode
+  g <- graph_from_adjacency_matrix(.mat,
+                                   mode = ifelse(.directed == FALSE,
+                                                 "undirected",
+                                                 "directed"))
+  g
 }
 
 #' @title Transform to one-mode
@@ -56,68 +59,30 @@ to_graph <- function(.mat, directed=FALSE, ...){
 #' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
 #'
 #' @param .g, An `igraph` object produced by the `to_graph` function.
-#' @param auto, Set whether this function automatically transforms data or do you control it. This is a logical argument.
 #' @param project, Projection type for igraph to extract. "rows" will extract a one-mode matrix of rows-to-rows, while "columns" will extract a one-mode matrix of colums-to-columns.
 #' 
 #' @importFrom igraph is.bipartite bipartite.mapping is.igraph bipartite.projection V
 #'
 #' @export
-to_one_mode <- function(.g, auto=TRUE, project="rows", ...){
-  if(!is.igraph(.g)){
+to_one_mode <- function(.g, project="rows", ...) {
+  if (!is.igraph(.g)) {
     stop("Object provided is not an igraph object, try again.", call. = FALSE)
   }
-  if(!is.logical(auto)){
-    stop("'auto' value provided is not a logical, only TRUE and FALSE logicals are accepted.", call. = FALSE)
-  }
-  if(!is.character(project)){
+  if (!is.character(project) && project != "rows" | project != "columns") {
     stop("'project' value provided is not a string, only 'rows' and 'columns' strings are accepted.", call. = FALSE)
   }
-  if(project == "rows" | project == "columns"){
-  if(is.igraph(.g)){
-    if(auto==TRUE){
-      if(is.bipartite(.g)==FALSE){
-        return(.g)
-      }
-      if(is.bipartite(.g)==TRUE){
-        if(project=="rows"){
-          g <- bipartite.projection(.g)$proj1
-        }
-        if(project=="columns"){
-          g <- bipartite.projection(.g)$proj2
-        }
-        return(g)
-      }
+  
+  if (is.bipartite(g)) {
+    if (project == "rows") {
+      g <- bipartite.projection(.g)$proj1
     }
-    if(auto==FALSE){
-      if(is.bipartite(.g)==FALSE){
-        cat("Graph is one-mode, no need to transform.")
-        return(.g)
-      }
-      if(is.bipartite(.g)==TRUE){
-        choice <- menu(c("Yes", "No"), title=paste("Your network", deparse(substitute(.g)), "tested as bipartite, is this true?"))
-        if(choice==1){
-          V(.g)$type <- bipartite.mapping(.g)$type
-          type_choice <- menu(c("Row -- Row (proj1)", "Column -- Column (proj2)"), title=paste("Select how to project the two-mode network."))
-          if(type_choice==1){
-            g <- bipartite.projection(.g)$proj1
-          }
-          if(type_choice==2){
-            g <- bipartite.projection(.g)$proj2
-          }
-          return(g)
-        }
-        if(choice==2){
-          cat("Cool, not a bipartate network, so no need to transform it")
-          return(.g)
-        }
-      }
+    if (project == "columns") {
+      g <- bipartite.projection(.g)$proj2
     }
   }
-  }
-  else {
-    stop("'project' value provided is not valid, only 'rows' and 'columns' strings are accepted.", call. = FALSE)
-  }
-  }
+  
+  g
+}
 
 #' @title Read Relational Excels
 #'
@@ -126,18 +91,25 @@ to_one_mode <- function(.g, auto=TRUE, project="rows", ...){
 #' @param .path, A path to the workbook with multiple relational and non-relational network data.
 #' 
 #' @importFrom readxl excel_sheets read_excel
-#' @importFrom tidyselect ends_with
-#' @importFrom purrr map
+#' @importFrom purrr map set_names
+#' @importFrom magrittr %>%
 #' 
 #' @usage 
 #' listed_df <- get_xlsx("datasets/Afghan Tribal Networks.xlsx")
 #' 
 #' @export
-get_xlsx <- function(.path){
-  if(endsWith(basename(.path), "xlsx")){
-    sheets <- readxl::excel_sheets(.path)
-    listed_dfs <- purrr::map(sheets, function(X) readxl::read_excel(.path, sheet = X))
-    names(listed_dfs) <- sheets
-    return(listed_dfs)
+extract_xlsx <- function(.path) {
+  if (!endsWith(basename(.path), "xlsx")) {
+    stop("Path provided did not end with the .xlsx extension expercted", call. = FALSE)
   }
-}
+  
+  listed_tibbles <- .path %>%
+    excel_sheets() %>%
+    set_names() %>%
+    map(
+      ~ readxl::read_excel(path = .path, sheet = .x) %>%
+        dplyr::as_tibble()
+    )
+  
+  listed_tibbles
+  }
