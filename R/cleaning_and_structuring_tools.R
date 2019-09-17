d70fead0cdc9d24cce177831700ad89c7c1dcf88
cjcallag <cjcallag@nps.edu>
2019-09-17 16:23:05 -0700

    Changed the workflow from import to namespacing and removed exporting.

--- R/cleaning_and_structuring_tools.R
+++ R/cleaning_and_structuring_tools.R
@@ -5,64 +5,28 @@
 #'
 #' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
 #'
-#' @param .df, A `df` produced by the `read_xlsx` function.
-#' @param .rownames, A column value for the row that will be reassigned as 
+#' @param df, A `df` produced by the `read_xlsx` function.
+#' @param rownames, A column value for the row that will be reassigned as 
 #' rownames and then removed from the data frame.
-#'
-#' @export
-to_matrix <- function(.df, .rownames = 1) {
-  if (!is.data.frame(.df)) {
+#' 
+to_matrix <- function(df, rownames = 1) {
+  if (!is.data.frame(df)) {
     stop("Not a data frame.",
          call. = FALSE)
   }
-  if (!is.numeric(.rownames)) {
+  if (!is.numeric(rownames)) {
     stop("rownames value provided in not numeric",
          call. = FALSE)
   }
   
-  .df <- as.matrix(.df)
-  row.names(.df) <- .df[, .rownames]
-  .df <- .df[, -.rownames]
+  df <- as.matrix(df)
+  row.names(df) <- df[, rownames]
+  df <- df[, -rownames]
   
-  if (NROW(.df) != NCOL(.df)) message("An incidence matrix will be returned")
-  if (NROW(.df) == NCOL(.df)) message("An adjacency matrix will be returned.")
+  if (NROW(df) != NCOL(df)) message("An incidence matrix will be returned")
+  if (NROW(df) == NCOL(df)) message("An adjacency matrix will be returned.")
   
-  .df
-}
-
-#' @title Graph Matrices as `igraph` graphs
-#' 
-#' @description `to_graph` returns an `igraph` object when provided with a 
-#' matrix. The function evaluates the matrix and graphs either a bipartite 
-#' graph or not.`
-#'
-#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
-#'
-#' @param .mat, A `matrix` produced by the `to_adj_matrix` function.
-#' @param directed, Logical argument to specify whether or not a network is to 
-#' graphed as directed or not.
-#' 
-#' @importFrom igraph graph_from_adjacency_matrix graph_from_incidence_matrix
-#'
-#' @export
-to_graph <- function(.mat, .directed=FALSE, ...) {
-  if (!is.matrix(.mat)==TRUE) {
-    stop("Object provided is not a matrix.",
-         call. = FALSE)
-  }
-  
-  # Two-mode data
-  if (NROW(.mat) != NCOL(.mat)) {
-    g <- graph_from_incidence_matrix(.mat)
-    return(g)
-  }
-  
-  # One-mode
-  g <- graph_from_adjacency_matrix(.mat,
-                                   mode = ifelse(.directed == FALSE,
-                                                 "undirected",
-                                                 "directed"))
-  g
+  df
 }
 
 #' @title Transform to one-mode
@@ -73,17 +37,15 @@ to_graph <- function(.mat, .directed=FALSE, ...) {
 #'
 #' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
 #'
-#' @param .g, An `igraph` object produced by the `to_graph` function.
+#' @param g, An `igraph` object produced by the `to_graph` function.
 #' @param project, Projection type for igraph to extract. "rows" will extract a 
 #' one-mode matrix of rows-to-rows, while "columns" will extract a one-mode 
 #' matrix of colums-to-columns.
-#' 
-#' @importFrom igraph is.bipartite bipartite.mapping is.igraph bipartite.projection V
 #'
-#' @export
-to_one_mode <- function(.g, project = "rows", ...) {
-  if (!is.igraph(.g)) {
-    stop("Object provided is not an igraph object, try again.", call. = FALSE)
+to_one_mode <- function(g, project = "rows", ...) {
+  if (!is.igraph(g)) {
+    stop("Object provided is not an igraph object, try again.",
+         call. = FALSE)
   }
   if (!is.character(project)) {
     stop("project value provided is not a string.",
@@ -97,16 +59,16 @@ to_one_mode <- function(.g, project = "rows", ...) {
          call. = FALSE)
   }
   
-  if (is.bipartite(.g)) {
+  if (igraph::is.bipartite(g)) {
     if (project == "rows") {
-      .g <- bipartite.projection(.g)$proj1
+      g <- igraph::bipartite.projection(g)$proj1
     }
     if (project == "columns") {
-      .g <- bipartite.projection(.g)$proj2
+      g <- igraph::bipartite.projection(g)$proj2
     }
   }
 
-  .g
+  g
 }
 
 #' @title Read Multiple Tables from Excels
@@ -116,210 +78,27 @@ to_one_mode <- function(.g, project = "rows", ...) {
 #' @description `extract_xlsx` inspects excel documents for multiple tabs, then
 #' proceeds to open these, and return all in a list as tibbles.
 #'
-#' @param .path, A path to the workbook with multiple relational and 
+#' @param path, A path to the workbook with multiple relational and 
 #' non-relational network data.
-#' 
-#' @importFrom readxl excel_sheets read_excel
-#' @importFrom purrr map set_names
-#' @importFrom magrittr %>%
 #' 
 #' @usage 
 #' listed_df <- get_xlsx("datasets/Afghan Tribal Networks.xlsx")
 #' 
-#' @export
-extract_xlsx <- function(.path) {
-  if (!endsWith(basename(.path), "xlsx")) {
+extract_xlsx <- function(path) {
+  if (!endsWith(basename(path), "xlsx")) {
     stop("Path provided did not end with the .xlsx extension expected",
          call. = FALSE)
   }
   
-  listed_tibbles <- .path %>%
-    excel_sheets() %>%
-    set_names() %>%
-    map(
-      ~ readxl::read_excel(path = .path, sheet = .x) %>%
+  `%>%` <- magrittr::`%>%`
+  
+  listed_tibbles <- path %>%
+    readxl::excel_sheets() %>%
+    purrr::set_names() %>%
+    purrr::map(
+      ~ readxl::read_excel(path = path, sheet = .x) %>%
         dplyr::as_tibble()
     )
   
   listed_tibbles
 }
-
-#' @title Unnest Igraph Object by Edge Type
-#'
-#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
-#' 
-#' @description `unnest_edge_types` extracts subnetworks based on the edge_type.
-#'
-#' @param igraph_graph, An igraph object.
-#' @param edge_type_attr_name, String denoting edge type variable.
-#' 
-#' @importFrom igraph edge_attr subgraph.edges is_igraph edge.attributes
-#' 
-#' @usage 
-#' df <- data.frame(from = c("a", "a", "b"), to = c("a", "b", "c"), from_class = "agent", to_class = "agent", edge_type = c("self", "foo", "bar"))
-#' unnest_edge_types(g, "edge_type")
-#' 
-#' @export
-unnest_edge_types <- function(igraph_graph, edge_type_attr_name) {
-  
-  if (!is_igraph(igraph_graph)) {
-    stop("Graph provided is not and igraph object.",
-         call. = FALSE)
-  }
-  if (!is.character(edge_type_attr_name) || edge_type_attr_name %!in% names(edge.attributes(igraph_graph))) {
-    stop("edge_type_attr_name must as string and a valid edge attribute for the graph object.",
-         call. = FALSE)
-  }
-  
-  types <- unique(igraph::edge_attr(graph = igraph_graph,
-                                    name  = edge_type_attr_name)
-                  )
-  listed_graphs <- lapply(types,
-         function(x) subgraph.edges(igraph_graph,
-                                    eids = which(
-                                      igraph::E(igraph_graph)$edge_type %in% x)
-                                    )
-         )
-  listed_graphs
-}
-
-#' @title Generate COREnets Graph Metadata
-#'
-#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
-#'
-#' @param igraph_graph, An igraph object.
-#' 
-#' @importFrom igraph is_bipartite is_directed any_multiple is_weighted ecount vcount as_data_frame
-#' 
-#' @usage 
-#' df <- data.frame(from = c("a", "a", "b"), to = c("a", "b", "c"), from_class = "agent", to_class = "agent", edge_type = c("self", "foo", "bar"))
-#' g <- graph_from_data_frame(df)
-#' unnest_edge_types(g, "edge_type") %>%
-#'    purrr::map(~.x %>%
-#'               generate_graph_metadata)
-#'               
-#' @export
-generate_graph_metadata <- function(igraph_graph) {
-  metadata <- list(
-    list(
-      graph_metadata = list(
-        is_bimodal   = igraph::is_bipartite(igraph_graph),
-        is_directed  = igraph::is_directed(igraph_graph),
-        is_dynamic   = test_dynamic(igraph_graph),
-        is_multiplex = igraph::any_multiple(igraph_graph),
-        is_weighted  = igraph::is_weighted(igraph_graph),
-        has_loops    = test_loops(igraph_graph),
-        has_isolates = test_isolates(igraph_graph)
-      ),
-      edges_metadata = list(
-        count       = ecount(igraph_graph),
-        are_dynamic = test_dynamic(igraph_graph)
-        
-        # TODO g? 
-        # TODO igraph can't return sf objects
-        # are_spatial = inherits(igraph::as_data_frame(g,
-                                                     # what = "edges"),
-                               # "sf")
-      ),
-      nodes_metadata = list(
-        count         = vcount(igraph_graph),
-        classes       = get_node_classes(igraph_graph),
-        classes_count = length(get_node_classes(igraph_graph))
-        
-        # TODO igraph can't retrun sf objects
-        # are_spatial   = inherits(igraph::as_data_frame(igraph_graph,
-                                                       # what = "vertices"),
-                                 # "sf")
-      )
-      )
-    )
-  names(metadata) <- unique(igraph::edge_attr(graph = igraph_graph,
-                                              name  = "edge_type")
-  )
-  # Returns
-  metadata
-}
-
-#' @title Testing for Loops
-#'
-#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
-#' 
-#' @importFrom igraph is_igraph is.loop
-#' 
-#' @export
-test_loops <- function(igraph_graph) {
-  if (!is_igraph(igraph_graph)) {
-    stop("Graph provided is not and igraph object.",
-         call. = FALSE)
-  }
-  
-  (TRUE %in% is.loop(igraph_graph))
-}
-
-#' @title Testing for Isolates
-#'
-#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
-#' 
-#' @importFrom igraph is_igraph simplify degree
-#' 
-#' @export
-test_isolates <- function(igraph_graph) {
-  if (!is_igraph(igraph_graph)) {
-    stop("Graph provided is not and igraph object.",
-         call. = FALSE)
-  }
-  
-  temp <- igraph::simplify(igraph_graph,
-                   remove.loops = TRUE)
-  (0 %in% degree(temp))
-}
-
-
-#' @title Testing for Isolates
-#'
-#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
-#' 
-#' @importFrom igraph is_igraph edge_attr
-#'
-get_node_classes <- function(igraph_graph, from_class = "from_class", to_class = "to_class") {
-  if (!is_igraph(igraph_graph)) {
-    stop("Graph provided is not and igraph object.",
-         call. = FALSE)
-  }
-  if (!is.character(from_class) || from_class %!in% names(edge.attributes(igraph_graph))) {
-    stop("from_class must as string and a valid edge attribute for the graph object.",
-         call. = FALSE)
-  }
-  if (!is.character(to_class) || to_class %!in% names(edge.attributes(igraph_graph))) {
-    stop("to_class must as string and a valid edge attribute for the graph object.",
-         call. = FALSE)
-  }
-  
-  classes <- unique(
-    c(
-      edge_attr(graph = igraph_graph,
-                name  = from_class),
-      edge_attr(graph = igraph_graph,
-                name  = to_class)
-  )
-  )
-  # Return
-  classes
-}
-
-#' @title Testing for Dynamic Edges
-#'
-#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
-#' 
-#' @importFrom igraph is_igraph edge_attr
-#'
-test_dynamic <- function(igraph_graph) {
-  if (!is_igraph(igraph_graph)) {
-    stop("Graph provided is not and igraph object.",
-         call. = FALSE)
-  }
-  atts <- names(igraph::edge_attr(igraph_graph))
-  "edge_time" %in% atts
-  }
-
-
---
+++ R/metadata_functions.R
@@ -0,0 +1,196 @@
+#' @title Unnest Igraph Object by Edge Type
+#'
+#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
+#' 
+#' @description `unnest_edge_types` extracts subnetworks based on the edge_type.
+#'
+#' @param g, An igraph object.
+#' @param edge_type_name, String denoting edge type variable.
+#' 
+unnest_edge_types <- function(g, edge_type_name) {
+  
+  if (!igraph::is_igraph(g)) {
+    stop("Graph provided is not and igraph object.",
+         call. = FALSE)
+  }
+  if (!is.character(edge_type_name) || edge_type_name %!in% names(igraph::edge.attributes(g))) {
+    stop("edge_type_attr_name must as string and a valid edge attribute for the graph object.",
+         call. = FALSE)
+  }
+  
+  types <- unique(igraph::edge_attr(graph = g,
+                                    name  = edge_type_name)
+  )
+  listed_graphs <- lapply(types,
+                          function(x) igraph::subgraph.edges(g,
+                                                             eids = which(
+                                                               igraph::E(g)$edge_type %in% x)
+                          )
+  )
+  listed_graphs
+}
+
+#' @title Generate COREnets Graph Metadata
+#'
+#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
+#'
+#' @param g, An `igraph` object.
+#' @param codebook, A `data.frame` with metedata fields for each `edge_type` 
+#' in the graph object.
+#'               
+generate_graph_metadata <- function(g, codebook) {
+  if (!igraph::is_igraph(g)) {
+    stop("Graph provided is not and igraph object.",
+         call. = FALSE)
+  }
+  if (!is.data.frame(codebook)) {
+    stop("The codebook provided is not a data.frame object.",
+         call. = FALSE)
+  }
+  
+  fields <- get_codebook_fields(codebook = codebook,
+                                wanted_edge_type = unique(
+                                  igraph::edge_attr(
+                                    graph = g,
+                                    name  = "edge_type")
+                                  )
+                                )
+  
+  metadata <- list(
+    list(
+      graph_metadata = list(
+        is_bimodal   = fields[["is_bimodal"]],
+        is_directed  = fields[["is_directed"]],
+        is_dynamic   = fields[["is_dynamic"]],
+        is_weighted  = fields[["is_weighted"]],
+        has_loops    = test_loops(g),
+        has_isolates = test_isolates(g)
+      ),
+      edges_metadata = list(
+        count       = igraph::ecount(g),
+        are_dynamic = fields[["is_dynamic"]]
+        
+        # TODO g? 
+        # TODO igraph can't return sf objects
+        # are_spatial = inherits(igraph::as_data_frame(g,
+        # what = "edges"),
+        # "sf")
+      ),
+      nodes_metadata = list(
+        count         = igraph::vcount(g),
+        classes       = get_node_classes(g),
+        classes_count = length(get_node_classes(g))
+        
+        # TODO igraph can't retrun sf objects
+        # are_spatial   = inherits(igraph::as_data_frame(g,
+        # what = "vertices"),
+        # "sf")
+      )
+    )
+  )
+  names(metadata) <- unique(
+    igraph::edge_attr(
+      graph = g,
+      name  = "edge_type")
+  )
+  # Returns
+  metadata
+}
+
+
+#' @title Return Metadata Fields from Codebook
+#'
+#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
+#' 
+get_codebook_fields <- function(codebook, wanted_edge_type) {
+  if (!is.character(wanted_edge_type)) {
+    stop("The edge_type argument provided is not a string.",
+         call. = FALSE)
+  }
+  if (!is.data.frame(codebook)) {
+    stop("The codebook provided is not a data.frame object.",
+         call. = FALSE)
+  }
+  
+  filtered_codebook <- codebook[codebook[["edge_type"]] == wanted_edge_type, ]
+  
+  if (NROW(filtered_codebook) != 1) {
+    stop("Returned filtered_codebook contains unexpected number of rows.",
+         call. = FALSE)
+  }
+  
+  filtered_codebook
+}
+
+
+#' @title Testing for Loops
+#'
+#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
+#' 
+test_loops <- function(g) {
+  if (!igraph::is_igraph(g)) {
+    stop("Graph provided is not and igraph object.",
+         call. = FALSE)
+  }
+  
+  (TRUE %in% igraph::is.loop(g))
+}
+
+
+#' @title Testing for Isolates
+#'
+#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
+#' 
+test_isolates <- function(g) {
+  if (!igraph::is_igraph(g)) {
+    stop("Graph provided is not and igraph object.",
+         call. = FALSE)
+  }
+  
+  temp <- igraph::simplify(g,
+                           remove.loops = TRUE)
+  (0 %in% igraph::degree(temp))
+}
+
+
+#' @title Testing for Isolates
+#'
+#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
+#'
+get_node_classes <- function(g, from_class = "from_class", to_class = "to_class") {
+  if (!igraph::is_igraph(g)) {
+    stop("Graph provided is not and igraph object.",
+         call. = FALSE)
+  }
+  if (!is.character(from_class) || from_class %!in% names(igraph::edge.attributes(g))) {
+    stop("from_class must as string and a valid edge attribute for the graph object.",
+         call. = FALSE)
+  }
+  if (!is.character(to_class) || to_class %!in% names(igraph::edge.attributes(g))) {
+    stop("to_class must as string and a valid edge attribute for the graph object.",
+         call. = FALSE)
+  }
+  
+  classes <- unique(
+    c(
+      igraph::edge_attr(graph = g,
+                        name  = from_class),
+      igraph::edge_attr(graph = g,
+                        name  = to_class)
+    )
+  )
+  classes
+}
+
+#' @title Testing for Dynamic Edges
+#'
+#' @author Christopher Callaghan, \email{cjcallag@@nps.edu}
+#'
+test_dynamic <- function(g) {
+  if (!igraph::is_igraph(g)) {
+    stop("Graph provided is not and igraph object.",
+         call. = FALSE)
+  }
+  atts <- names(igraph::edge_attr(g))
+  "edge_time" %in% atts
+}
