9b6b60a1a448ca6c81ff9273666df498d9439f29
cjcallag <cjcallag@nps.edu>
2019-09-18 10:07:26 -0700

    Metadata_functions testing

--- tests/testthat/test-metadata_functions.R
+++ tests/testthat/test-metadata_functions.R
@@ -32,35 +32,67 @@ test_that("unnest_edge_types() works", {
   expect_type(listed_g,
               "list")
   # Each item in list is igraph class?
-  expect_equal(purrr::map_chr(listed_g,
-                              class),
-               rep("igraph",
-                   length(listed_g)
-                   )
-               )
+  expect_equal(
+    sapply(
+      listed_g,
+      class),
+    rep("igraph",
+        length(listed_g)
+        )
+    )
   # Contains edge_type field?
-  expect_true("edge_type" %in% names(igraph::edge_attr(listed_g[[1]])))
+  expect_true(
+    "edge_type" %in% names(
+      igraph::edge_attr(
+        listed_g[[1]]
+        )
+      )
+    )
 })
 
 test_that("get_codebook_fields() works", {
   # Returns data.frame?
-  expect_s3_class(get_codebook_fields(codebook,
-                                      "foo"),
-                  "data.frame")
+  expect_s3_class(
+    get_codebook_fields(
+      codebook,
+      "foo"),
+    "data.frame")
   # Returns expected headers?
-  expect_equal(names(get_codebook_fields(codebook,
-                                   "foo")),
-               c("edge_type",
-                 "is_bimodal",
-                 "is_directed",
-                 "is_dynamic",
-                 "is_weighted",
-                 "definition"))
+  expect_equal(
+    names(
+      get_codebook_fields(
+        codebook,
+        "foo")
+      ),
+    c("edge_type",
+      "is_bimodal",
+      "is_directed",
+      "is_dynamic",
+      "is_weighted",
+      "definition")
+    )
   # Returns expected dims?
   expect_equal(
-    dim(get_codebook_fields(codebook,
-                             "foo")),
-    c(1, 6)
+    dim(
+      get_codebook_fields(
+        codebook,
+        "foo")
+      ),
+    c(1,
+      6)
+  )
+  # Returns expected types?
+  expect_equal(
+    sapply(get_codebook_fields(
+      codebook,
+      "foo"),
+      typeof),
+    c(edge_type   = "character",
+      is_bimodal  = "logical",
+      is_directed = "logical",
+      is_dynamic  = "logical",
+      is_weighted = "logical",
+      definition  = "character")
   )
 })
 
@@ -68,29 +100,90 @@ test_that("generate_graph_metadata() works", {
   # Is the output a list?
   expect_type(
     purrr::map(listed_g,
-               ~generate_graph_metadata(.x,
-                                        codebook = codebook)),
+           ~ generate_graph_metadata(.x,
+                                     codebook = codebook)),
     "list")
   # Each item on that is three in lenght?
   expect_equal(
     length(
       purrr::map(
         listed_g,
-        ~generate_graph_metadata(.x,
-                                 codebook = codebook))[[1]][[1]]),
+        ~ generate_graph_metadata(.x,
+                                 codebook = codebook))[[1]][[1]])
+    ,
     3)
   # Names of the three listed items match expected schema:
   expect_equal(
     names(
-      purrr::map(listed_g,
-                 ~generate_graph_metadata(.x,
-                                          codebook = codebook)
-                 )[[1]][[1]]),
+      purrr::map(
+        listed_g,
+        ~ generate_graph_metadata(.x,
+                                  codebook = codebook))[[1]][[1]]
+      ),
     c("graph_metadata",
       "edges_metadata",
-      "nodes_metadata"))
+      "nodes_metadata")
+    )
   # Names of those graph_metadata match expectation?
+  expect_equal(
+    sapply(
+      purrr::map(
+        listed_g,
+        ~ generate_graph_metadata(.x,
+                                  codebook = codebook))[[1]][[1]][["graph_metadata"]],
+      typeof),
+    c(is_bimodal   = "logical",
+      is_directed  = "logical",
+      is_dynamic   = "logical",
+      is_weighted  = "logical",
+      has_loops    = "logical",
+      has_isolates = "logical")
+  )
   # Names of those edges_metadata match expectation?
+  expect_equal(
+    sapply(
+      purrr::map(
+        listed_g,
+        ~ generate_graph_metadata(.x,
+                                  codebook = codebook))[[1]][[1]][["edges_metadata"]],
+      typeof),
+    c(count       = "double",
+      are_dynamic = "logical")
+  )
   # Names of those nodes_metadata match expectation?
-  
+  expect_equal(
+    sapply(
+      purrr::map(
+        listed_g,
+        ~ generate_graph_metadata(.x,
+                                  codebook = codebook))[[1]][[1]][["nodes_metadata"]],
+      typeof),
+    c(count         = "integer",
+      classes       = "character",
+      classes_count = "integer")
+  )
+})
+
+test_that("test_loops() works", {
+  expect_true(
+    test_loops(g)
+    )
+  expect_false(
+    test_loops(
+      igraph::simplify(g, remove.loops = TRUE)
+    )
+    )
+})
+
+test_that("test_isolates() works", {
+  expect_true(
+    test_isolates(
+      igraph::add_vertices(g,
+                           1,
+                           name = "d")
+      )
+  )
+  expect_false(
+    test_isolates(g)
+  )
 })
